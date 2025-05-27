d4f4a9e7ca2ec1c446a9f58434c7ed47545b8da5
irinasvetlichnaya <irinasvetlichnaya16@gmail.com>
2025-05-28 02:14:23 +0300

    add App CafeInfo

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import css from './App.module.css'
+import CafeInfo from '../CafeInfo/CafeInfo'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+export default function App() {
+
+  const [clicks, setClicks] = useState<number>(0);
+  // const [votes, setVotes] = useState(0)
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log("I'm a button!", event);
+    setClicks(clicks + 1);
+    console.log(clicks);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={css.app}>
+        <CafeInfo />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={handleClick}>Good</button>
+      <button onClick={handleClick}>Neutral</button>
+      <button onClick={handleClick}>Bad</button>
     </>
   )
+  // const [count, setCount] = useState(0)
+
+  // return (
+  //   <>
+  //     <div>
+  //       <a href="https://vite.dev" target="_blank">
+  //         <img src={viteLogo} className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://react.dev" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </>
+  // )
 }
 
-export default App
